0546d35d7a4a89391e7a21bd065bc88773aa3c2a
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2009-10-05 17:33:59 -0500

    Fixed issue where admin attribute wasn't accesible

--- lib/clearance_admin/admin/user.rb
+++ lib/clearance_admin/admin/user.rb
@@ -2,17 +2,31 @@ module Clearance
   module Admin  
     module User
 
-        def self.included(model)
-          model.send(:include, InstanceMethods)
-        end
+      def self.included(model)
+         model.send(:include, InstanceMethods)
+         model.send(:include, AttrAccessor)
+       end
 
-        module InstanceMethods
-      
-          def admin?
-            self.admin
-          end
-      
-        end    
+       module InstanceMethods
+     
+         def admin?
+           self.admin
+         end
+     
+       end    
+
+
+		    module AttrAccessor
+		      # Hook for attr_accessor virtual attributes.
+		      #
+		      # :password, :password_confirmation
+		      def self.included(model)
+		        model.class_eval do
+		          attr_accessor :admin
+		        end
+		      end
+		    end
+
     end
   end
 end
